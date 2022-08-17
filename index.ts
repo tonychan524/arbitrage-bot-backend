8064a4e3d35395ad213e8c233421cfac6f7ec014
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-08-17 01:38:39 -0400

    added dexs router and basetokens

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { Wallet } from "@ethersproject/wallet";
-import { JsonRpcProvider } from "@ethersproject/providers";
+import { JsonRpcProvider, WebSocketProvider } from "@ethersproject/providers";
 import { formatEther, formatUnits } from "@ethersproject/units";
 import Sever from "bunrest";
-
+import axios from "axios";
 import { ArbAggregator } from "lib/ArbFactory";
+import BscData from 'data/bsc.json'
+
 import {
   init,
   getParameters,
@@ -12,7 +14,7 @@ import {
 } from "./lib/database";
 
 init();
-initializeParameter();
+// initializeParameter();
 
 const config = {
   PRIVATE_KEY: process.env.PRIVATE_KEY,
@@ -21,27 +23,15 @@ const config = {
   TARGET_TOKEN_SYMBOL: process.env.TARGET_TOKEN_SYMBOL,
   BLOCKCHAIN_PROVIDER: process.env.BLOCKCHAIN_PROVIDER,
   TARGET_TOKEN_DECIMAL: process.env.TARGET_TOKEN_DECIMAL,
+  WEBSOCKET_PROVIDER: process.env.WEBSOCKET_PROVIDER,
   ARB_CONTRACT: process.env.ArbContract,
 };
+console.log(config.BLOCKCHAIN_PROVIDER);
 
-const provider = new JsonRpcProvider(config.BLOCKCHAIN_PROVIDER);
-
+const provider = new WebSocketProvider(config.WEBSOCKET_PROVIDER);
 const signer = new Wallet(config.PRIVATE_KEY, provider);
-
 const arbContract = ArbAggregator.connect(config.ARB_CONTRACT, signer);
 
-const getMarketData = async () => {
-  console.log("starting to get market data...");
-  const result = await fetch(
-    `${config.DEX_SCREENER_API}/token/${config.TARGET_TOKEN_ADDRESS}`
-  );
-  console.log("finished to get market data...");
-  console.log(result);
-  return result;
-};
-const run = async () => {
-  // await getMarketData()
-};
 // API Endpoints
 const app = Sever();
 
@@ -51,10 +41,6 @@ app.get("/api/get_config", (req, res) => {
   res.status(200).json(result);
 });
 
-app.get("/api/get_market_data", async (req, res) => {
-  const result = await getMarketData();
-  res.status(200).json(result);
-});
 app.get("/api/get_target_token", (req, res) => {
   console.log("requested to get target token info");
   res.status(200).json({
@@ -64,16 +50,17 @@ app.get("/api/get_target_token", (req, res) => {
   });
 });
 
-app.get("/api/get_balance", async (req, res) => {
-  const balance = await provider.getBalance(config.ARB_CONTRACT);
-  const tokenBalance = await arbContract.getBalance(
-    config.TARGET_TOKEN_ADDRESS
-  );
-  res.status(200).json({
-    bnb: formatEther(balance),
-    token: formatUnits(tokenBalance, config.TARGET_TOKEN_DECIMAL),
-  });
+app.get("/api/get_base_token", (req, res) => {
+  console.log("requested to get target token info");
+  res.status(200).json(BscData.baseAssets);
 });
+
+app.post("/api/save_config", (req, res) => {
+  console.log(req);
+  // setParameter(req.body);
+  res.status(200).json({ message: "succeed" });
+});
+
 app.post("/api/save_config", (req, res) => {
   console.log(req);
   // setParameter(req.body);
@@ -85,5 +72,3 @@ app.listen(3001, () => {
 });
 
 console.log("Hello via Bun!");
-
-run();
