ba5cd54791e0d8171222eab86593947a2174a22b
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-08-18 23:16:05 +0900

    initial commit for node-version bot

--- index.ts
+++ index.ts
@@ -1,24 +1,16 @@
-import { Wallet } from "@ethersproject/wallet";
+import { Wallet } from '@ethersproject/wallet'
 import {
   TransactionReceipt,
   TransactionResponse,
-  WebSocketProvider,
-} from "@ethersproject/providers";
-import { formatEther, formatUnits, parseEther } from "@ethersproject/units";
-import Sever from "bunrest";
-import { ArbAggregator } from "lib/ArbFactory";
-import BscData from "data/bsc.json";
+  JsonRpcProvider,
+} from '@ethersproject/providers'
+import { formatEther, formatUnits, parseEther } from '@ethersproject/units'
+import { ArbAggregator } from './lib/ArbFactory.js'
+import * as BscData from './data/bsc.json'
+const dotenv = require('dotenv')
+const fs = require('fs')
 
-import {
-  init,
-  getParameters,
-  setParameter,
-  getBotHistories,
-  addBotHistory,
-} from "./lib/database";
-
-init();
-// initializeParameter();
+dotenv.config()
 
 const config = {
   PRIVATE_KEY: process.env.PRIVATE_KEY,
@@ -29,44 +21,51 @@ const config = {
   TARGET_TOKEN_DECIMAL: process.env.TARGET_TOKEN_DECIMAL,
   WEBSOCKET_PROVIDER: process.env.WEBSOCKET_PROVIDER,
   ARB_CONTRACT: process.env.ArbContract,
-};
-let baseTokenAddress = BscData.baseAssets[0];
-let botinterval;
-const provider = new WebSocketProvider(config.WEBSOCKET_PROVIDER);
-const signer = new Wallet(config.PRIVATE_KEY, provider);
+}
+let baseTokenAddress = BscData.baseAssets[0]
+let botinterval
+const provider = new JsonRpcProvider(config.BLOCKCHAIN_PROVIDER)
+const signer = new Wallet(config.PRIVATE_KEY, provider)
 
-const arbContract = ArbAggregator.connect(config.ARB_CONTRACT, signer);
+const arbContract = ArbAggregator.connect(config.ARB_CONTRACT, signer)
 
-// API Endpoints
-const app = Sever();
+const bot_config = {
+  slippage: 10,
+  gas_price: 10,
+  gas_limit: 10,
+  profit: 10,
+  liquidity: 10,
+  time_limit: 10,
+  bnb_amount: 10,
+}
 
 const startBot = (_config) => {
-  console.log("Starting bot...");
+  console.log('Starting bot...')
   botinterval = setInterval(async () => {
-    let max = 0;
-    let min = Math.pow(10, 5);
-    let min_index = 0; // best sell dex
-    let max_index = 0; // best buy dex
+    let max = 0
+    let min = Math.pow(10, 5)
+    let min_index = 0 // best sell dex
+    let max_index = 0 // best buy dex
     for (let i = 0; i < BscData.routers.length; i++) {
       try {
         const amtBack = await arbContract.getAmountOutMin(
           BscData.routers[i],
           baseTokenAddress,
           config.TARGET_TOKEN_ADDRESS,
-          _config.bnb_amount
-        );
-        const output = formatUnits(amtBack, config.TARGET_TOKEN_DECIMAL);
+          _config.bnb_amount,
+        )
+        const output = formatUnits(amtBack, config.TARGET_TOKEN_DECIMAL)
         // find best dexs for buying and selling
         if (parseFloat(output) > max) {
-          max = parseFloat(output);
-          max_index = i;
+          max = parseFloat(output)
+          max_index = i
         }
         if (parseFloat(output) < min) {
-          min = parseFloat(output);
-          min_index = i;
+          min = parseFloat(output)
+          min_index = i
         }
       } catch {
-        continue;
+        continue
       }
     }
 
@@ -79,106 +78,57 @@ const startBot = (_config) => {
           BscData.routers[min_index],
           baseTokenAddress,
           config.TARGET_TOKEN_ADDRESS,
-          _config.bnb_amount
-        );
+          _config.bnb_amount,
+        )
         // convert output amount into human readable value.
-        const output = formatEther(amtBack);
+        const output = formatEther(amtBack)
         if (
           parseFloat(output) - _config.bnb_amount >
           (_config.bnb_amount * _config.profit) / 100
         ) {
-          console.log("Starting bot transaction...");
+          console.log('Starting bot transaction...')
           const tx: TransactionResponse = await arbContract
             .dualTrade(
               BscData.routers[max_index],
               BscData.routers[min_index],
               baseTokenAddress,
               config.TARGET_TOKEN_ADDRESS,
-              _config.bnb_amount
+              _config.bnb_amount,
             )
             .send({
               gasLimit: _config.gas_limit,
               gasPrice: _config.gas_price,
-            });
-          const result: TransactionReceipt = await tx.wait();
+            })
+          const result: TransactionReceipt = await tx.wait()
 
-          addBotHistory({
-            txHash: result.transactionHash,
-            dex_from: BscData.routers[max_index].dex,
-            dex_to: BscData.routers[min_index].dex,
-            base_token: baseTokenAddress.sym,
-            target_token: config.TARGET_TOKEN_SYMBOL,
-            amount_in: _config.bnb_amount,
-            amount_out: parseFloat(output),
-            profit: (parseFloat(output) / _config.bnb_amount) * 100,
-            gas_used: parseInt(result.gasUsed.toString()),
-            datetime: new Date("yyyy-mm-dd HH:ii:ss"),
-          });
+          try {
+            const writeStream = fs.createWriteStream('bot_history.txt', {
+              flags: 'a',
+            })
+            writeStream.write(`
+            ********** BOT TRANSACTION RESULT START **************** \n
+            txHash: ${result.transactionHash}\n
+            dex_from: ${BscData.routers[max_index].dex}\n
+            dex_to: ${BscData.routers[min_index].dex}\n
+            base_token: ${baseTokenAddress.sym}\n
+            target_token: ${config.TARGET_TOKEN_SYMBOL}\n
+            amount_in: ${_config.bnb_amount}\n
+            amount_out: ${parseFloat(output)}\n
+            profit: ${(parseFloat(output) / _config.bnb_amount) * 100}\n
+            gas_used: ${parseInt(result.gasUsed.toString())}\n
+            datetime: ${new Date('yyyy-mm-dd HH:ii:ss')}\n
+            ********** BOT TRANSACTION RESULT END **************** \n
+            `)
+            writeStream.end()
+          } catch {
+            console.log('failed to write file.')
+          }
         }
       } catch {
-        console.log("failed to execute transaction.");
+        console.log('failed to execute transaction.')
       }
     }
-  }, parseInt(_config.timelimit) * 1000 );
-};
+  }, parseInt(_config.timelimit) * 1000)
+}
 
-// get bot parameters
-app.get("/api/get_config", (req, res) => {
-  const result = getParameters();
-  res.status(200).json(result);
-});
-
-app.get("/api/get_target_token", (req, res) => {
-  console.log("requested to get target token info");
-  res.status(200).json({
-    address: config.TARGET_TOKEN_ADDRESS,
-    symbol: config.TARGET_TOKEN_SYMBOL,
-    decimal: config.TARGET_TOKEN_DECIMAL,
-  });
-});
-
-app.get("/api/get_base_token", (req, res) => {
-  console.log("requested to get target token info");
-  res.status(200).json(BscData.baseAssets);
-});
-
-app.post("/api/save_config", async (req, res) => {
-  console.log(await req.text());
-  setParameter(JSON.parse(await req.text()));
-  if (botinterval) clearInterval(botinterval);
-  const config = getParameters();
-  startBot(config);
-  res.status(200).json({ message: "succeed" });
-});
-
-app.post("/api/change_base_token", async (req, res) => {
-  console.log(await req.text());
-  const params = JSON.parse(await req.text());
-  baseTokenAddress = BscData.baseAssets.filter(
-    (basetoken) => basetoken.sym == params.token
-  )[0];
-
-  const bot_config = getParameters();
-  if (botinterval) clearInterval(botinterval);
-  startBot(bot_config);
-
-  res.status(200).json({ message: "succeed" });
-});
-app.get("/api/get_bot_histories", (req, res) => {
-  const histories = getBotHistories();
-  // setParameter(req.body);
-  if (histories.length > 0) {
-    res.status(200).json(histories);
-  } else {
-    res.status(200).json({});
-  }
-});
-app.listen(3001, () => {
-  console.log("App is listening on port 3001");
-});
-
-console.log("Hello via Bun!");
-
-if (botinterval) clearInterval(botinterval);
-const botConfig = getParameters();
-startBot(botConfig);
+startBot(bot_config)
